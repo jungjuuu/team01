0199257ca99a0630e914ed61a09fca0f6861b79b
yujin-kim <qkqh5682@naver.com>
2024-09-17 22:31:35 +0900

    상품 결제 구현
    Controller, MemberDAO, mapper, buyproduct.html, productDetail.html 수정

--- src/main/java/com/cream/team01/controller/BuyController.java
+++ src/main/java/com/cream/team01/controller/BuyController.java
@@ -1,16 +1,51 @@
 package com.cream.team01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cream.team01.dao.MemberDAO;
+import com.cream.team01.dao.ProductDAO;
+import com.cream.team01.vo.MemberVO;
+import com.cream.team01.vo.ProductVO;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class BuyController {
 
-	@RequestMapping(value = {"/buy"})
-	public ModelAndView getBuyProduct() {
-		ModelAndView result = new ModelAndView("/buyproduct"); 
-		return result;
+	@Autowired
+	private ProductDAO productDAO;
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	
+	
+	@Autowired
+	HttpSession session;
+	
+	@RequestMapping("/buyproduct/{productNo}")
+	public ModelAndView getBuyProduct(@PathVariable("productNo") int productNo) {
+		
+		int memberNo = (Integer)session.getAttribute("memberNo");
+		
+		MemberVO member = memberDAO.getMemberInfo(memberNo);
+		
+		ProductVO buyproduct = productDAO.getProductByNo(productNo);
+		
+		ModelAndView result = new ModelAndView("buyproduct"); 
+		
+		
+		result.addObject("buyproduct", buyproduct);
+		result.addObject("member", member);
+
+		return result; 
+		
 	}
+	
+	
 	
 }
