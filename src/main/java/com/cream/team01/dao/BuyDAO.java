7014c18f699274cc3d3e9c58c41a9f5a6168d6c0
yujin-kim <qkqh5682@naver.com>
2024-09-18 22:13:45 +0900

    잠시 커밋

--- src/main/java/com/cream/team01/dao/BuyDAO.java
+++ src/main/java/com/cream/team01/dao/BuyDAO.java
@@ -3,12 +3,26 @@ package com.cream.team01.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cream.team01.vo.OrderVO;
 
 @Component
 public class BuyDAO {
 
 	@Autowired
 	SqlSession session;
+
+	public void addOrder(OrderVO order) {
+
+		session.insert("addOrder", order);
+	}
+	
+	public void addOrderDetail(OrderVO orderDetail) {
+		
+		session.insert("addOrderDetail", orderDetail);
+	}
+
 	
 	
 }
