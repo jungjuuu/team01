fd28d00436e1dde5b63b386849b21277b99a6cee
minjung <ddw01309@naver.com>
2024-09-18 17:19:19 +0900

    회원가입 null 일때 submit 안되는 기능 추가

--- src/main/java/com/cream/team01/controller/RegisterController.java
+++ src/main/java/com/cream/team01/controller/RegisterController.java
@@ -1,6 +1,25 @@
 package com.cream.team01.controller;
 
 public class RegisterController {
+	
+	@Autowired
+    private RegisterDAO registerDAO;
+	
+	// 멤버 회원가입
+	@RequestMapping("/memberregister")
+    public ModelAndView addMemberRegisterForm( 
+    		@ModelAttribute AccountVO account, 
+            @ModelAttribute MemberVO member){ 
+		
+		 registerDAO.createAccount(account);  
+		 member.setAccountNo(account.getAccountNo());   
+		 registerDAO.createMember(member);
+		 
+		
+		 
+        ModelAndView result = new ModelAndView("/memberregister"); 
+        return result;
+    }
 
 	public static void main(String[] args) {
 		
