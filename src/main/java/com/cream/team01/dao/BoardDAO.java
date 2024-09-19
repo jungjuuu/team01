8c5828f26e470da93639b577ff514340e67e81aa
minjung <ddw01309@naver.com>
2024-09-19 18:55:57 +0900

    1:1 문의하기

--- src/main/java/com/cream/team01/dao/BoardDAO.java
+++ src/main/java/com/cream/team01/dao/BoardDAO.java
@@ -1,17 +1,14 @@
 package com.cream.team01.dao;
 
-import java.util.List;
 
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.cream.team01.vo.AccountVO;
 import com.cream.team01.vo.BoardVO;
-import com.cream.team01.vo.MemberVO;
 import com.cream.team01.vo.ProductVO;
-import com.cream.team01.vo.SellerVO;
 
 @Component
 public class BoardDAO {
@@ -20,11 +17,10 @@ public class BoardDAO {
     SqlSession session;
 
     // 문의하기
-    public void creatememberInquriyWrite(BoardVO board) {
+    public void addMemberInquiryWrite(BoardVO board)  {
         session.insert("addMemberInquiryWrite", board);
     }
     
-
 
 }
 
