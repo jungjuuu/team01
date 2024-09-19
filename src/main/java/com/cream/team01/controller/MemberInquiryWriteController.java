2537163a08655479546313a556b64309d7ceb3b9
minjung <ddw01309@naver.com>
2024-09-19 11:40:06 +0900

    BoardDAO 추가

--- src/main/java/com/cream/team01/controller/MemberInquiryWriteController.java
+++ src/main/java/com/cream/team01/controller/MemberInquiryWriteController.java
@@ -1,14 +1,27 @@
 package com.cream.team01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cream.team01.dao.BoardDAO;
+import com.cream.team01.vo.BoardVO;
 
 @Controller
 public class MemberInquiryWriteController {
-	//123
-	@RequestMapping(value= {"/memberinquirywrite"})
-	public String main() {
-		return "memberinquirywrite";
+	
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	@RequestMapping("/memberinquirywrite")
+	public ModelAndView addMemberInquiryWriteForm(
+			@ModelAttribute BoardVO board) {
+		
+		boardDAO.creatememberInquriyWrite(board);
+		ModelAndView result = new ModelAndView("/memberinquirywrtie"); 
+        return result;
 	}
 	
 
