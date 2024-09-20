e4626b0fc786d43b326117cc4d066ea8b1540aaa
Hwang-JiHwan <hwang017_@naver.com>
2024-09-20 12:09:48 +0900

    회원가입

--- src/main/java/com/cream/team01/controller/RegisterController.java
+++ src/main/java/com/cream/team01/controller/RegisterController.java
@@ -1,12 +1,10 @@
 package com.cream.team01.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.cream.team01.dao.ProductDAO;
 import com.cream.team01.dao.RegisterDAO;
 import com.cream.team01.vo.AccountVO;
 import com.cream.team01.vo.MemberVO;
@@ -14,43 +12,37 @@ import com.cream.team01.vo.SellerVO;
 
 @Controller
 public class RegisterController {
-	
-	@Autowired
+
+    @Autowired
     private RegisterDAO registerDAO;
-	
-	// 멤버 회원가입
-	@RequestMapping("/memberregister")
-    public ModelAndView addMemberRegisterForm( 
-    		@ModelAttribute AccountVO account, 
-            @ModelAttribute MemberVO member){ 
-		
-		 registerDAO.createAccount(account);  
-		 member.setAccountNo(account.getAccountNo());   
-		 registerDAO.createMember(member);
-		 
-		
-		 
-        ModelAndView result = new ModelAndView("/memberregister"); 
-        return result;
+
+    // 멤버 회원가입 폼을 제공하는 GET 요청
+    @RequestMapping(value = "/memberregister", method = RequestMethod.GET)
+    public String showMemberRegisterForm() {
+        return "memberregister"; // 회원가입 폼 페이지 반환
     }
 
-
-	// 브랜드 회원가입
-	@RequestMapping("/sellerregister")
-    public ModelAndView showSellerRegisterForm( 
-    		@ModelAttribute AccountVO account, 
-            @ModelAttribute SellerVO seller){ 
-		
-		 registerDAO.createAccount(account);  
-		 seller.setAccountNo(account.getAccountNo());   
-		 registerDAO.createBrand(seller);
-		 
-        ModelAndView result = new ModelAndView("/sellerregister"); 
-        return result;
+    // 멤버 회원가입 처리 (POST 요청)
+    @RequestMapping(value = "/memberregister", method = RequestMethod.POST)
+    public String addMemberRegisterForm(@ModelAttribute AccountVO account, @ModelAttribute MemberVO member) {
+        registerDAO.createAccount(account);
+        member.setAccountNo(account.getAccountNo());
+        registerDAO.createMember(member);
+        return "redirect:/accountlogin"; // 회원가입 후 로그인 페이지로 리다이렉션
     }
 
-	
-	
-	
+    // 브랜드 회원가입 폼을 제공하는 GET 요청
+    @RequestMapping(value = "/sellerregister", method = RequestMethod.GET)
+    public String showSellerRegisterForm() {
+        return "sellerregister"; // 브랜드 회원가입 폼 페이지 반환
+    }
+
+    // 브랜드 회원가입 처리 (POST 요청)
+    @RequestMapping(value = "/sellerregister", method = RequestMethod.POST)
+    public String addSellerRegisterForm(@ModelAttribute AccountVO account, @ModelAttribute SellerVO seller) {
+        registerDAO.createAccount(account);
+        seller.setAccountNo(account.getAccountNo());
+        registerDAO.createBrand(seller);
+        return "redirect:/accountlogin"; // 회원가입 후 로그인 페이지로 리다이렉션
+    }
 }
-
